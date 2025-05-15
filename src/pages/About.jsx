dbceddd503f65943e3ba46a0ebcf8d476cb24492
Dimitri <115943634+Dimtiri19@users.noreply.github.com>
2025-05-15 11:59:41 +0200

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,33 @@
-import React, { useEffect, useState } from 'react'
+import maPhoto from '../assets/gmt.jpg';
+import React, { useEffect, useState } from 'react';
 
 function About() {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:3000/about') // ou ton endpoint
       .then(res => res.json())
-      .then(data => setData(data))
-  }, [])
+      .then(data => setData(data));
+  }, []);
 
-  if (!data) return <div>hello guys...</div>
+  if (!data) return <div>hello guys...</div>;
 
   return (
-    <div>
+    <div style={{ padding: '2rem' }}>
       <h1>À propos</h1>
+      <img
+        src={maPhoto}
+        alt="Illustration à propos"
+        style={{
+          width: '300px',
+          borderRadius: '12px',
+          margin: '1rem 0',
+          display: 'block'
+        }}
+      />
       <p>{data.content}</p>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
