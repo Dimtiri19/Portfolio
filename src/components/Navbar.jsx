c32840b9ddc4ba6bc87b79f293eabbb85fdd1726
Dimitri <115943634+Dimtiri19@users.noreply.github.com>
2025-05-15 10:24:31 +0200

    Update Navbar color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,36 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav style={{ display: 'flex', gap: '10px', marginBottom: '20px' }}>
-      <Link to="/">Accueil</Link>
-      <Link to="/a-propos">À propos</Link>
-      <Link to="/contact">Contact</Link>
+    <nav
+      style={{
+        display: 'flex',
+        gap: '10px',
+        marginBottom: '20px',
+        backgroundColor: '#536878',
+        padding: '10px'
+      }}
+    >
+      <Link
+        to="/"
+        style={{ color: '#EAE0C8', textDecoration: 'none' }}
+      >
+        Accueil
+      </Link>
+      <Link
+        to="/a-propos"
+        style={{ color: '#EAE0C8', textDecoration: 'none' }}
+      >
+        À propos
+      </Link>
+      <Link
+        to="/contact"
+        style={{ color: '#EAE0C8', textDecoration: 'none' }}
+      >
+        Contact
+      </Link>
     </nav>
   )
 }
 
 export default Navbar
+
